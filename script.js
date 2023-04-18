0cef5fb3a1be42f5898d875d2d6baaf848cbc900
Mazuc0 <mateus.lima.nogue23@gmail.com>
2023-04-18 00:10:14 -0300

    comentarios

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+//inserir números ao resultado
 
 function insert(num) {
     var numero = document.getElementById("resultado").innerHTML;
     document.getElementById("resultado").innerHTML = numero + num;
 }
+//limpar o resultado com o "C"
 
 function clean() {
     document.getElementById('resultado').innerHTML = "";
 }
 
+//limpar apenas uma casa
 function back() {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
 }
+
+//Pegar o resultado e calcular
 
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
